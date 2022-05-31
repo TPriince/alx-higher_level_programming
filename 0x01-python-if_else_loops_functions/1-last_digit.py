1759671a47e694bfc20c82cbcec4b1cd59687bc2
TPriince <tobiloba110@gmail.com>
2022-05-31 11:05:39 -0700

    Task 1 - program that prints the last digit of the number stored

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number = str(number)
-last_digit = number[-1]
-last_digit = int(last_digit)
-number = int(number)
+if number < 0:
+    number = str(number)
+    last_digit = number[-1]
+    last_digit = int('-' + last_digit)
+    number = int(number)
+
+else:
+    number = str(number)
+    last_digit = number[-1]
+    last_digit = int(last_digit)
+    number = int(number)
+
+
 if last_digit == 0:
     print(f"Last digit of {number:d} is {last_digit:d} and is 0")
 
