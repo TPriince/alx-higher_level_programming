ecabb3f34979c356e374b286b3abe3973ac07486
TPriince <tobiloba110@gmail.com>
2022-06-04 14:51:59 -0700

    Task 11 - function that deletes an item at a specific position in a list

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def delete_at(my_list, idx=0):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+
+    else:
+        for i in range(0, len(my_list)):
+            if i == idx:
+                my_list.remove(my_list[idx])
+
+        return my_list
