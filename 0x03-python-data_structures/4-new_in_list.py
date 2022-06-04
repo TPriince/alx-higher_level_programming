9bd60355442c53ab401b12a2eff8eeb471a071d7
TPriince <tobiloba110@gmail.com>
2022-06-04 08:36:22 -0700

    Task 4 - function that replaces an element in a list at a specifc position without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,9 @@ def new_in_list(my_list, idx, element):
     if idx < 0 and idx > len(my_list) - 1:
         return my_list
 
+    elif not my_list:
+        pass
+
     else:
         n_list = my_list[:]
 
