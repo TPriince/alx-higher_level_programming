01a76fc37dd585a8f740fb89894a677536ab6292
TPriince <tobiloba110@gmail.com>
2022-06-08 15:05:51 -0700

    really hope this works

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+
+    if a_dictionary is None:
+        return None
+
+    else:
+        biggest_scoree = max(a_dictionary)
+        return biggest_scoree
