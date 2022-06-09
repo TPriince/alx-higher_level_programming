8c38fb0eccb7684e51bf2eb2732bc8740650e78b
TPriince <tobiloba110@gmail.com>
2022-06-08 21:01:20 -0700

    :heavy_check_mark: Task 12 - function that converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
-#!/usr/bib/python3
+#!/usr/bin/python3
 def roman_to_int(roman_string):
     digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
 
-    if not isinstance(roman_string, str) or roman_string is None:
+    if type(roman_string) is not str:
         return 0
     prev = 0
     sum = 0
