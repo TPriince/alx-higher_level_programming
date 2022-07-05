4193f38c87762f35ce0e179d73f443840a119f7b
TPriince <tobiloba110@gmail.com>
2022-07-05 16:15:37 -0700

    Task 1 - Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,7 @@ Writes in a text file
 
 
 def write_file(filename="", text=""):
-    """Writes text in filename.
+    """Writes text in filename
     Args:
         - filename: name of the file
         - text: string to write in the file
