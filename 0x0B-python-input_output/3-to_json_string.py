e7fd91f37c8d27b8f609fcf6cb3bb63377405a46
TPriince <tobiloba110@gmail.com>
2022-07-05 16:25:52 -0700

    Task 3 - To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+3-to_json_string module
+Returns the JSON representation of an object.
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns th JSON representation of my_obj.
+    Args:
+        - my_obj: string to represent
+    Returns: JSON representation
+    """
+
+    return json.dumps(my_obj)
