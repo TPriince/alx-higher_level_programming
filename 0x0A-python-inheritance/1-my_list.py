7850bf0d093f3434ff93de86405cd08602ec9df3
TPriince <tobiloba110@gmail.com>
2022-07-05 15:23:43 -0700

    Task 1 - My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+1-my_list module
+Creates a class inheriting from the list class.
+"""
+
+
+class MyList(list):
+    """Class MyList inherits from list."""
+
+    def print_sorted(self):
+        """Prints the list, in ascending sort."""
+
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
